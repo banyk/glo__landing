dd559932161709f2db1a389cf1793302c73f1dba
Bayan Bokan <beckbokanov@gmail.com>
2021-02-22 18:34:04 +0600

    timer done

---
+++ scripts/script.js
@@ -0,0 +1,53 @@
+/* eslint-disable strict */
+window.addEventListener('DOMContentLoaded', () => {
+	'use strict';
+
+	const countTimer = deadline => {
+		const timerHours = document.getElementById('timer-hours'),
+			timerMinutes = document.getElementById('timer-minutes'),
+			timerSeconds = document.getElementById('timer-seconds');
+
+		const getTimeRemaining = () => {
+			const dateStop = new Date(deadline).getTime(),
+				dateNow = new Date().getTime(),
+				timeRemaining = (dateStop - dateNow) / 1000,
+				seconds = Math.floor(timeRemaining % 60),
+				minutes = Math.floor((timeRemaining / 60) % 60),
+				hours = Math.floor(timeRemaining / 3600);
+
+			return {
+				seconds,
+				minutes,
+				hours,
+				timeRemaining
+			};
+		};
+
+		const updateClock = () => {
+			const timer = getTimeRemaining();
+
+			if (timer.timeRemaining <= 0) {
+				timerHours.textContent = '00';
+				timerMinutes.textContent = '00';
+				timerSeconds.textContent = '00';
+				return false;
+			} else if (timer.timeRemaining > 0) {
+				for (let key in timer) {
+					if (timer[key] < 10) {
+						timer[key] = '0' + timer[key];
+					}
+				}
+				timerHours.textContent = timer.hours;
+				timerMinutes.textContent = timer.minutes;
+				timerSeconds.textContent = timer.seconds;
+			}
+
+		};
+		let idInterval;
+		updateClock() !== false ? idInterval = setInterval(updateClock, 1000) : clearInterval(idInterval);
+
+	};
+
+	countTimer('23 feb 2021');
+
+});
